93dedc507d91a92957357ee60d2d1b52b26b253c
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-04-22 23:09:37 -0300

    feat: add new method in customer.

--- src/infra/repository/memory/CustomerRepositoryInMemory.ts
+++ src/infra/repository/memory/CustomerRepositoryInMemory.ts
@@ -1,7 +1,9 @@
 import { Customer } from "../../../models/Customer";
+import { Uuid } from "../../../models/Uuid";
 import { CustomerRepository } from "../../../models/repositories/CustomerRepository";
 
 export class CustomerRepositoryInMemory implements CustomerRepository {
+  
   private customerCollection: Array<Customer> = [];
 
   async save(customer: Customer): Promise<void> {
@@ -10,5 +12,13 @@ export class CustomerRepositoryInMemory implements CustomerRepository {
 
   async getAll(): Promise<Customer[]> {
     return this.customerCollection;
+  }
+  
+  async getById(id: Uuid): Promise<Customer> {
+    throw new Error('Method not implemented.')
+  }
+
+  async remove(id: Uuid): Promise<void> {
+    throw new Error("Method not implemented.");
   }
 }

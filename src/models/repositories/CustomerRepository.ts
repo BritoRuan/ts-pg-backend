ac08c322ec676998c7513157b45bbc330e3b90ab
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-04-22 23:10:14 -0300

    feat: add new method in interface.

--- src/models/repositories/CustomerRepository.ts
+++ src/models/repositories/CustomerRepository.ts
@@ -1,7 +1,9 @@
 import { Customer } from "../Customer";
+import { Uuid } from "../Uuid";
 
 export interface CustomerRepository {
   save(customer: Customer): Promise<void>;
-
   getAll(): Promise<Customer[]>;
+  getById(id: Uuid): Promise<Customer>
+  remove(id: Uuid): Promise<void>
 }
